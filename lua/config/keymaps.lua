e3db1c8e529400d059911acd0c4ca6c9cbd59879
Azaleee <oshoffpro@gmail.com>
2025-10-06 14:35:50 +0200

    Add shortcut and indent convention

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -38,3 +38,27 @@ vim.keymap.set("n", "<leader>lw", "<cmd>set wrap!<CR>", opts)
 
 -- Replace word under cursor
 vim.keymap.set("n", "<leader>j", "*``cgn", opts)
+
+if vim.g.neovide then
+  -- Toggle plein écran avec Alt+Entrée
+  vim.keymap.set("n", "<A-CR>", function()
+    vim.g.neovide_fullscreen = not vim.g.neovide_fullscreen
+  end, { desc = "Toggle fullscreen in Neovide" })
+end
+
+-- Indentation intuitive en mode visuel
+vim.keymap.set("v", "<Tab>", ">gv", { desc = "Indent selection" })
+vim.keymap.set("v", "<S-Tab>", "<gv", { desc = "Unindent selection" })
+
+-- === Déplacement de lignes / blocs ===
+-- Mode normal
+vim.keymap.set("n", "<A-Up>", ":m .-2<CR>==", { desc = "Move line up" })
+vim.keymap.set("n", "<A-Down>", ":m .+1<CR>==", { desc = "Move line down" })
+
+-- Mode visuel
+vim.keymap.set("v", "<A-Up>", ":m '<-2<CR>gv=gv", { desc = "Move block up" })
+vim.keymap.set("v", "<A-Down>", ":m '>+1<CR>gv=gv", { desc = "Move block down" })
+
+-- Naviguer entre les buffers ouverts
+vim.keymap.set("n", "<C-,>", ":bprevious<CR>", { desc = "Previous buffer" })
+vim.keymap.set("n", "<C-.>", ":bnext<CR>", { desc = "Next buffer" })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -50,3 +50,27 @@ vim.api.nvim_create_autocmd("ColorScheme", {
   group = vim.api.nvim_create_augroup("NeovideFloatBG", { clear = true }),
   callback = clear_float_bg,
 })
+
+-- Exemple : C# = 4 espaces
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "cs", "csharp" },
+  callback = function()
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.softtabstop = 4
+    vim.opt_local.expandtab = true
+  end,
+})
+
+-- Exemple : Lua = 2 espaces
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "lua" },
+  callback = function()
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.expandtab = true
+  end,
+})
+
+
